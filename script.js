8c0ed7bce8ab42fcb05a000e4ef35924e1c41961
Joseph <60448358+joebuggy561@users.noreply.github.com>
2025-02-01 17:18:11 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSideBar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  
+}
+
+function sideBarClose(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
